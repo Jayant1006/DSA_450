39937b258e4935698a62a7c44bc59bc2ea08e765
Jayant1006 <jayantnadar124@gmail.com>
2021-01-12 20:55:02 +0530

    Day-1

---
+++ maxAndmin.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	
+	int a[n];
+	
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	cout<<*max_element(a,a+n)<<endl;
+	cout<<*min_element(a,a+n)<<endl;
+}
